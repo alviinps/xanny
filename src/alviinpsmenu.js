900069f89f6abda12496900a68b409865ab5df0b
alviinps <80861085+alviinps@users.noreply.github.com>
2021-03-18 14:19:12 -0300

    Add files via upload

---
+++ src/alviinpsmenu.js
@@ -0,0 +1,40 @@
+const alviinpsmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do ALVIINPS:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  TRADUZIDO POR *ALVIINPS*
+  DUVIDAS? 👇
+  WA.me/557184274015
+╚════════════════════`
+}
+
+exports.alviinpsmenu = alviinpsmenu
+
+
+
+
+
+
+
+
